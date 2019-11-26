d395c8eced5bad28e3fafc31937ad8e337360813
Adrian Garcia <adriangarlop@hotmail.com>
2019-11-26 16:05:09 -0600

    Added main structure. As well as button event listener

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// Declare memory arrays
+let M = new Array(2048);
+let S = new Array(4096);
+
+function 
+
+function main() {
+
+	// Click on input button
+	$("#input-btn").on("click", function() {
+
+		// Store data that user set
+		let query = $("#input").val();
+		let i=0;
+
+		// If user put spaces at first, the program fix this and get the first letter
+		while (query[i] == ' ') i++;
+		query = query.substr(i, query.length);
+
+		// Go to the function that the string correspond
+		switch(query[i]) {
+			
+			case 'p':
+			break;
+			
+			case 'a':
+			break;
+		}
+	});
+}
+main();
