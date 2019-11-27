6249d117dfd5e89296640467f9930121e934060f
Adrian Garcia <adriangarlop@hotmail.com>
2019-11-26 18:37:12 -0600

    Added a, p, l, c, f and e functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 // Declare memory arrays
-let M = new Array(2048);
+let M = new Array(2048);	
 let S = new Array(4096);
+
+let result = [];
+
+function accessMemory(query) {
+	result.push("accessMemory");
+}
+
+function freeSpace(query) {
+	result.push("freeSpace");
+}
+
+function loadProcess(query) {
+	result.push("loadProcess");
+}
+
+function addComment(query) {
+	result.push("addComment");
+}
+
+function appendCode() {
+
+	$(".result-content").empty();
+
+	for (let i=0; i<result.length; i++) {
+		$(".result-content").append(`
+			<div class="result-line-output">${result[i]}</div>
+		`);
+	}
+}
 
 function main() {
 
@@ -9,20 +38,57 @@ function main() {
 
 		// Store data that user set
 		let query = $("#input").val();
-		let i=0;
 
-		// If user put spaces at first, the program fix this and get the first letter
-		while (query[i] == ' ') i++;
-		query = query.substr(i, query.length);
+		// Split array line by line
+		query = query.split('\n');
+
+		// If user put spaces at first, the program fix this and get the first letter of every line
+		for (let i=0; i<query.length; i++) {
+			let j=0;
+			while (query[i][j] == ' ') j++;
+			query[i] = query[i].substr(j, query[i].length);
+		}
+
+		result = [];		
 
 		// Go to the function that the string correspond
-		switch(query[i]) {
-			
-			case 'p':
-			break;
-			
-			case 'a':
-			break;
+		for (let i=0; i<query.length; i++) {
+
+			if (query[i] == "")
+				continue;
+
+			switch(query[i].toLowerCase()) {
+
+				// Acces virtual memory
+				case 'a':
+					accessMemory(query[i]);
+				break;
+
+				// Free pages at process "p"
+				case 'l':
+					freeSpace(query[i]);
+				break;
+
+				// Load proces on memory
+				case 'p':
+					loadProcess(query[i]);
+				break;
+
+				// Comment
+				case 'c':
+					addComment(query[i]);
+				break;
+
+				// End package of requests
+				case 'f':
+					appendCode();
+				break;
+
+				// End program
+				case 'e':
+					console.log("Pues bye");
+				break;
+			}
 		}
 	});
 }
