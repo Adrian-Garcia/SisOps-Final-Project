e8ba8d59e8e5fe562d5b15b763f1005272b88720
Humberto Gonzalez <hglez.98s@gmail.com>
2019-12-01 17:33:02 -0600

    AccessMemory progress

--- src/index.js
+++ src/index.js
@@ -28,6 +28,28 @@ function fillArray() {
 }
 
 function accessMemory(query) {
+    // Show user input
+    result.push(query);
+    // Show what the command is going to do
+    let instruction = 'Obtener la dirección real correspondiente a la dirección virtual ' + query[1] + 'del proceso ' + query[2];
+    let page = Math.floor(query[1] / 16);
+    if (query[3] == '1') {
+        instruction += ' y modificar dicha dirección.';
+        result.push(instruction);
+        // if the address is modified, notify the user what page of what process was changed
+        result.push('Página ' + page + ' del proceso ' + query[2] + ' modificada.');
+    }
+    else {
+        result.push(instruction);
+    }
+    let realAddress;
+    for (let i = 0; i < processes.length; i++) {
+        if (processes[i].name == query[2]) {
+            for (let f = 0; f < processes[i].frames.length; f++) {
+                
+            }
+        }
+    }
 
 	// Tienes que pushear el resultado. No seas pendejo y lo dejes así
 	result.push("accessMemory");
