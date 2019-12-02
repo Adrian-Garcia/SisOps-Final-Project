6a57362e172044eb397e67e7a3eddccd0a2be467
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2019-12-02 17:44:00 -0600

    Puse la carne en la parilla y empezó a hacer tssss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ let S = new Array(4096);
 let time = 0;
 let result = [];
 
+let usedProcesses = [];
+
 processes = [];
 // los maps son chidos
 let processesStartTime = new Map();
@@ -29,6 +31,35 @@ function fillArray() {
 			};
 		}
 	}
+}
+
+function updateLeastRecentlyUsed(process) {
+	//Check if the process is already used
+	for (let i = 0; i < usedProcesses.length; i++) {
+		if (usedProcesses[i] == process) 
+			usedProcesses.splice(i, 1);
+	}
+
+	//Update last recently used
+	usedProcesses.push(process);
+}
+
+function firstUsed() {
+	//Find the first frame of the least recently used
+	let lru = 0;
+	let first = false;
+	for (let i = 0; i < M.length / 16 && !first; i++) {
+		if (M[i * 16].name == usedProcesses[0]) {
+			lru = i * 16;
+			first = true;
+		}
+	}
+	//Find the first used frame of the least recently used
+	for (let i = lru / 16; i < M.length / 16; i++) {
+		if (M[i*16].name == usedProcesses[0] && M[i * 16].timeStamp < M[lru].timeStamp)
+			lru = i * 16;
+	}
+	return lru;
 }
 
 function firstIn() {
@@ -116,9 +147,86 @@ function fifo(pName, inVirtualMemory, page) {
     return frameToSwapPos / 16;
 }
 
+function lru(pName, inVirtualMemory, page) {
+	console.log('Entre a lru');
+	let frameUsed;
+	// Get the position of the least recently used frame that entered M
+	let frameToSwapPos = firstUsed();
+
+	/// Swap out the frame that first entered M
+	for (let i = 0; i < S.length; i++) {
+		if (!S[i].isOccupied) {
+			frameUsed = Math.floor(i / 16);
+			for (let j = i; j < i + 16; j++) {
+				S[j] = {
+					processName: M[frameToSwapPos].processName,
+					isOccupied: true,
+					timeStamp: time
+				};
+			}
+			time += 1;
+			console.log('Swapped out frame');
+			break;
+		}
+	}
+
+	/// Save the frame where it was swapped out
+	// Find the process
+	for (let i = 0; i < processes.length; i++) {
+		if (processes[i].name == M[frameToSwapPos].processName) {
+			console.log('process swapped out found.');
+			// Find the frame that was swapped out
+			for (let f = 0; f < processes[i].frames.length; f++) {
+				if (processes[i].frames[f] == frameToSwapPos / 16) {
+					console.log('frame swapped out found.');
+					// remove frame reference to real memory
+					processes[i].frames[f] = null;
+					// add frame reference to virtual memory
+					processes[i].virtualFrames[f] = frameUsed;
+
+					result.push('Página ' + f + ' del proceso ' + M[frameToSwapPos].processName + ' swappeada al marco ' + frameUsed + ' del área de swapping.');
+				}
+			}
+		}
+	}
+
+	/// Swap in the frame required
+	for (let i = frameToSwapPos; i < frameToSwapPos + 16; i++) {
+		M[i] = {
+			processName: pName,
+			isOccupied: true,
+			timeStamp: time
+		};
+	}
+	time += 1;
+
+	/// If the frame swapped in was in virtual memory, remove it
+	if (inVirtualMemory) {
+		for (let i = 0; i < processes.length; i++) {
+			if (processes[i].name == pName) {
+				if (S[processes[i].virtualFrames[page] * 16].isOccupied && S[processes[i].virtualFrames[page] * 16].processName == pName) {
+					for (let j = processes[i].virtualFrames[page] * 16; j < processes[i].virtualFrames[page] * 16 + 16; j++) {
+						S[j] = {
+							isOccupied: false
+						};
+					}
+				}
+				result.push('Se localizó la página ' + page + ' del proceso ' + pName + ' que estaba en la posición ' + processes[i].virtualFrames[page] * 16 + ' de swapping y se cargó al marco ' + frameToSwapPos / 16 + '.');
+				// add frame reference to real memory
+				processes[i].frames[page] = frameToSwapPos / 16;
+				// remove frame reference to virtual memory
+				processes[i].virtualFrames[page] = null;
+
+			}
+		}
+	}
+	return frameToSwapPos / 16;
+}
+
 function accessMemory(query) {
     // Show user input
 	result.push("<i>" + query[0] + " " + query[1] + " " + query[2] + " " + query[3] + "</i>");
+
     // Show what the command is going to do
     let instruction = '<b>Obtener la dirección real correspondiente a la dirección virtual ' + query[1] + ' del proceso ' + query[2] + '</b>';
     let page = Math.floor(query[1] / 16);
@@ -130,15 +238,26 @@ function accessMemory(query) {
     }
     else {
         result.push(instruction);
-    }
-    time += 0.1;
-    let realAddress;
+	}
+
+	//Update the process
+	updateLeastRecentlyUsed(query[2]);
+	
+	time += 0.1;
+	let realAddress;
+	
     for (let i = 0; i < processes.length; i++) {
         if (processes[i].name == query[2]) {
             // check if desired address isnt in real memory
             if (processes[i].frames[page] == null && processes[i].virtualFrames[page] != null){
-                // Do replacement algorithm to load it to real memory
-                fifo(processes[i].name, true, page);
+				// Do replacement algorithm to load it to real memory
+				if ($("#sel1").val() == "FIFO") {
+					fifo(processes[i].name, true, page);
+				}
+				else {
+					lru(processes[i].name, true, page);
+				}
+                
                 processesFaultNum.set(query[2], processesFaultNum.get(query[2]) + 1);
             }
             realAddress = (processes[i].frames[page] * 16) + (query[1] % 16) ;
@@ -186,24 +305,30 @@ function freeSpace(query) {
 		}
 	}
 
-	let realText = "Se liberan los marcos de memoria real: ";
-	for (let i = 0; i < framesToRelease.length; i++) {
-		realText += framesToRelease[i] + ", ";
+	if (framesToRelease.length > 0) { 
+		let realText = "Se liberan los marcos de memoria real: ";
+		for (let i = 0; i < framesToRelease.length; i++) {
+			realText += framesToRelease[i] + ", ";
+		}
+		realText = realText.substring(0, realText.length - 2);
+		//Push result
+		result.push(realText);
 	}
-	realText = realText.substring(0, realText.length - 2);
 
-	let virtualText = "Se liberan los marcos "
-	for (let i = 0; i < virtualFramesToRelease.length; i++) {
-		virtualText += virtualFramesToRelease[i] + ", ";
+	if (virtualFramesToRelease.length > 0) {
+		let virtualText = "Se liberan los marcos "
+		for (let i = 0; i < virtualFramesToRelease.length; i++) {
+			virtualText += virtualFramesToRelease[i] + ", ";
+		}
+		virtualText = virtualText.substring(0, virtualText.length - 2);
+		virtualText += " del área de swapping";
+		result.push(virtualText);
 	}
-	virtualText = virtualText.substring(0, virtualText.length - 2);
-	virtualText += " del área de swapping";
 
     // Calculate turnaround time and save it in map
-    processesTurnAround.set(query[1], time - processesStartTime.get(query[1]));
+	processesTurnAround.set(query[1], time - processesStartTime.get(query[1]));
+	
     //Push result
-	result.push(realText);
-	result.push(virtualText);
 	result.push("<div class='space-result'></div>");
 }
 
@@ -236,7 +361,12 @@ function loadProcess(query) {
     }
 	if (requiredFrames > 0) {
         for (let i = 0; i < requiredFrames; i++) {
-            framesToUse.push(fifo(query[2], false, framesToUse.length));
+			if ($("#sel1").val() == "FIFO") {
+				framesToUse.push(fifo(query[2], false, framesToUse.length));
+			}
+			else {
+				framesToUse.push(lru(query[2], false, framesToUse.length));
+			}
             // Increase page fault count for the process
             processesFaultNum.set(query[2], processesFaultNum.get(query[2]) + 1);
         }
@@ -244,6 +374,9 @@ function loadProcess(query) {
     
 	//Para ir guardando los procesos que se van usando y saber cuales estan ocupados
 	processes.push({name: query[2], frames: framesToUse, virtualFrames: new Array(Math.ceil(query[1] / 16))});
+
+	//Actualizar el ultimo proceso que fue utilizado
+	updateLeastRecentlyUsed(query[2]);
     
 
 	//Imprimir textito final
